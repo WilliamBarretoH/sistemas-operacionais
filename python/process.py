2dc93a102091988c23fc16daede77c354174a8a7
William <willbarretosan@gmail.com>
2021-03-15 19:32:47 -0300

    init

---
+++ python/process.py
@@ -0,0 +1,12 @@
+import os
+from datetime import datetime
+  
+n = os.fork() 
+# n greater than 0  means parent process 
+if n > 0: 
+    print("Parent process id is : ", os.getpid()) 
+# n equals to 0 means child process 
+else: 
+    print("Child process id is : ", os.getpid()) 
+
+print(datetime.now())
